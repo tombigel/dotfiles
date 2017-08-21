c4c0714ca5b730fb76ceee4f1d346b74d9fcfd08
Tom Bigelajzen <public@tombigel.com>
2017-08-21 23:01:47 +0300

    Create zshrc.sh

---
+++ zshrc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Update .zshrc with installed brew packages and with relevant plugins
+
+cat >> ~/test << EOF
+-plugins=(git github git-prompt brew brew-cask npm nvm jira sublime atom z grunt osx yarn zsh_reload)
+
+source $ZSH/oh-my-zsh.sh
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+export PATH="$HOME/.yarn/bin:$PATH"
+export NVM_DIR="$HOME/.nvm"
+  . "/usr/local/opt/nvm/nvm.sh"
+EOF
+
+
+
