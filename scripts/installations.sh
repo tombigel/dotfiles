6eefbfce571ca3bfc019a900a6f6f0aa433ed772
Tom Bigelajzen <public@tombigel.com>
2017-08-22 00:49:27 +0300

    Update installations.sh

--- scripts/installations.sh
+++ scripts/installations.sh
@@ -30,20 +30,35 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 echo "Installing brew-cask gui packages"
 
 brew cask install iterm2 
-brew cask install google-chrome 
-brew cask install opera 
-brew cask install firefox 
+brew cask install google-chrome caskroom/versions/google-chrome-canary
+brew cask install safari-technology-preview
+brew cask install opera opera-developer
+brew cask install firefox firefoxdeveloperedition
+
 brew cask install alfred 
 brew cask install bettertouchtool 
+
 brew cask install sublime-text 
 brew cask install webstorm
 brew cask install atom 
+brew cask install visual-studio-code
+
 brew cask install vlc 
+brew cask install subler 
 brew cask install handbrake 
+brew cask install plex-media-server
+brew cask install transmission 
+
 brew cask install teamviewer 
+
+brew cask install backblaze backblaze-downloader
 brew cask install dropbox 
+
 brew cask install whatsapp 
+brew cask install telegram
 brew cask install skype 
+
+brew cask install adobe-creative-cloud
 
 # Remove outdated versions from the cellar.
 brew cleanup
