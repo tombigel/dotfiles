88e1d9bfb6c24dad4f4d6e1910e8ab7bc5cbc6b9
Tom Bigelajzen <public@tombigel.com>
2017-08-22 11:12:06 +0300

    Update installations.sh

--- scripts/installations.sh
+++ scripts/installations.sh
@@ -30,35 +30,52 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # Install brew-cask packages
 echo "Installing brew-cask gui packages"
 
+# Terminal
 brew cask install iterm2 
-brew cask install google-chrome caskroom/versions/google-chrome-canary
-brew cask install safari-technology-preview
-brew cask install opera opera-developer
-brew cask install firefox firefoxdeveloperedition
 
+# Browsers
+brew cask install google-chrome 
+brew cask install opera
+brew cask install firefox
+
+# Dev Browsers
+brew cask install caskroom/versions/google-chrome-canary
+brew cask install safari-technology-preview
+brew cask install opera-developer
+brew cask install firefoxdeveloperedition
+
+# Network and Remote
+brew cask install teamviewer 
+brew cask install charles 
+
+# Productivity
 brew cask install alfred 
 brew cask install bettertouchtool 
 
+# IDEs and Editors
 brew cask install sublime-text 
 brew cask install webstorm
 brew cask install atom 
 brew cask install visual-studio-code
 
+# Media
 brew cask install vlc 
 brew cask install subler 
 brew cask install handbrake 
 brew cask install plex-media-server
 brew cask install transmission 
 
-brew cask install teamviewer 
-
-brew cask install backblaze backblaze-downloader
+# Backup and Cloud Storage
+brew cask install backblaze 
+brew cask install backblaze-downloader
 brew cask install dropbox 
 
+# Social
 brew cask install whatsapp 
 brew cask install telegram
 brew cask install skype 
 
+# Design
 brew cask install adobe-creative-cloud
 
 # Remove outdated versions from the cellar.
@@ -81,5 +98,6 @@ else
   # mas install 1176895641 #Spark
 fi
 
-
+echo "Backblaze installation only copied the installer, it needs to be manually installed"
+open '/usr/local/Caskroom/backblaze/latest/Backblaze Installer.app'
 
