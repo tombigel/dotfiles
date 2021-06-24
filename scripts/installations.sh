b0ef138b908090469f8644c91ed637b083990c49
Tom Bigelajzen <public@tombigel.com>
2021-06-24 02:57:51 -0700

    Update installations.sh

--- scripts/installations.sh
+++ scripts/installations.sh
@@ -4,7 +4,7 @@
 which -s brew
 if [[ $? != 0 ]] ; then
     echo "Installing Homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "Updating Homebrew"
     brew update
